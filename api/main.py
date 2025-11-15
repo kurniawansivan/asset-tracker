bd70cac7ebe1cb0dd71339c2c9fb648afa13e9e2
kurniawansivan <ivan@wolkk.com>
2025-11-15 23:07:16 +0800

    Add user router to main application for API integration

--- api/main.py
+++ api/main.py
@@ -2,8 +2,8 @@
 Main application file for the Asset Tracker API.
 """
 from fastapi import FastAPI
-from routers import asset_router
 from fastapi.middleware.cors import CORSMiddleware
+from routers import asset_router, user_router
 
 app = FastAPI(title="Asset Tracker API", version="1.0.0")
 
@@ -21,6 +21,7 @@ app.add_middleware(
 )
 
 app.include_router(asset_router.router)
+app.include_router(user_router.router)
 
 @app.get("/")
 def read_root():
