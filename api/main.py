e1cf729cd2a87c6a096566ee8e7ad012cc17a75f
kurniawansivan <ivan@wolkk.com>
2025-11-15 20:20:19 +0800

    Add CORS middleware to support cross-origin requests

--- api/main.py
+++ api/main.py
@@ -3,8 +3,22 @@ Main application file for the Asset Tracker API.
 """
 from fastapi import FastAPI
 from routers import asset_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Asset Tracker API", version="1.0.0")
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(asset_router.router)
 
