e5d25c72239b3e5b3510eb950089df68fe2cbb3f
kurniawansivan <ivan@wolkk.com>
2025-11-15 20:15:58 +0800

    Refactor asset fetching and define Asset interface in +page.svelte

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let assets = [];
+    interface Asset {
+        id: number;
+        name: string;
+        serial_number: string | null;
+        purchase_date: string; 
+        wwarranty_expires: string | null;
+    }
+
+	let assets: Asset[];
 
 	onMount(async () => {
-		const response = await fetch('/api/assets');
+		const response = await fetch('http://localhost:8000/api/assets');
 		const data = await response.json();
 		assets = data;
 	});
 </script>
 
 <h1>Welcome to Aegis</h1>
+
+{#each assets as asset}
+  <div>
+    <h2>{asset.name}</h2>
+    <p>Serial Number: {asset.serial_number}</p>
+    <p>Purchase Date: {asset.purchase_date}</p>
+  </div>
+{/each}
